90cb83d25e5717af0589022721eee3a8a8d86fee
Steffan Halvorsen <steffanhalv@gmail.com>
2015-06-10 16:22:39 +0200

    List files from folder

--- main.js
+++ main.js
@@ -1,89 +1,85 @@
 $(document).ready(function() {
 
-    $( ".explorer" ).resizable({
-        handles: "e",
-        resize: function(e, ui) {
-            $('.composer').css({
-                left: $('.explorer').width()
-            });
-            $('.player').css({
-                left: $('.explorer').width()
-            });
-            $( ".browser-top" ).css({
-                left: $('.explorer').width()
-            });
-            $( ".browser-line" ).css({
-                left: $('.explorer').width()
-            });
-        }
-    });
+  $( ".explorer" ).resizable({
+    handles: "e",
+    resize: function(e, ui) {
+      $('.composer').css({
+        left: $('.explorer').width()
+      });
+      $('.player').css({
+        left: $('.explorer').width()
+      });
+      $( ".browser-top" ).css({
+        left: $('.explorer').width()
+      });
+      $( ".browser-line" ).css({
+        left: $('.explorer').width()
+      });
+    }
+  });
 
-    $( ".audio" ).draggable({
-        revert: 'invalid',
-        helper: function(){
-            return '<div class="track">Track</div>'
-        },
-        containment: 'document',
-        cursor: 'move'
-    });
+  $( ".composer td" ).droppable({
+      accept: ".audio",
+      drop: function(e, ui) {
 
-    $( ".composer td" ).droppable({
-        accept: ".audio",
-        drop: function(e, ui) {
-            var trackId = createTrackId();
-            $(this).append('<div class="track" id="'+trackId+'">Track</div>');
-            $('#'+trackId).css({
-                position: 'absolute',
-                left: ui.position.left-$('.explorer').width()+$('.composer').scrollLeft()
-            }).draggable({
-                containment: '.composer',
-                grid: [ 1, 20 ]
-            }).resizable({
-                handles: "e, w"
-            }).attr({
-                source: 'test'
-            });
-        }
-    });
+        console.log(ui.helper[0]);
 
-    $( ".composer" ).scroll(function() {
-        $( ".browser-top" ).css({
-            marginLeft: -$('.composer').scrollLeft()
-        });
-        $( ".browser-line" ).css({
-            marginLeft: -$('.composer').scrollLeft()+10
-        });
-    });
+          var trackId = createTrackId();
+          $(this).append('<div class="track" id="'+trackId+'">'+ui.helper[0].innerHTML+'</div>');
+          $('#'+trackId).css({
+              position: 'absolute',
+              left: ui.position.left-$('.explorer').width()+$('.composer').scrollLeft()
+          }).draggable({
+              containment: '.composer',
+              grid: [ 1, 20 ]
+          }).resizable({
+              handles: "e, w"
+          }).attr({
+              source: 'test'
+          });
+      }
+  });
 
-    $( ".browser-line" ).draggable({
-        containment: '.composer',
-        axis: 'x',
-        drag: function(e, ui) {
-            $( ".browser-top").css({
-                left: $( ".browser-line").position().left
-            });
-        },
-        stop: function(e, ui) {
-            $( ".browser-top").css({
-                left: $( ".browser-line").position().left
-            });
-        }
-    });
+  $( ".composer" ).scroll(function() {
+      $( ".browser-top" ).css({
+          marginLeft: -$('.composer').scrollLeft()
+      });
+      $( ".browser-line" ).css({
+          marginLeft: -$('.composer').scrollLeft()+10
+      });
+  });
 
-    $( ".browser-top" ).draggable({
-        containment: '.composer',
-        axis: 'x',
-        drag: function(e, ui) {
-            $( ".browser-line").css({
-                left: $( ".browser-top").position().left
-            });
-        },
-        stop: function(e, ui) {
-            $( ".browser-line").css({
-                left: $( ".browser-top").position().left
-            });
-        }
-    });
+  $( ".browser-line" ).draggable({
+      containment: '.composer',
+      axis: 'x',
+      drag: function(e, ui) {
+          $( ".browser-top").css({
+              left: $( ".browser-line").position().left
+          });
+      },
+      stop: function(e, ui) {
+          $( ".browser-top").css({
+              left: $( ".browser-line").position().left
+          });
+      }
+  });
+
+  $( ".browser-top" ).draggable({
+      containment: '.composer',
+      axis: 'x',
+      drag: function(e, ui) {
+          $( ".browser-line").css({
+              left: $( ".browser-top").position().left
+          });
+      },
+      stop: function(e, ui) {
+          $( ".browser-line").css({
+              left: $( ".browser-top").position().left
+          });
+      }
+  });
+
+  listFiles();
 
 });
 
@@ -92,3 +88,39 @@ var createTrackId = function() {
     id++;
     return 'track_'+id;
 }
+
+var listFiles = function() {
+
+  $.ajax({
+    type: "GET",
+    url: "functions/get_files.php",
+    dataType: "json",
+    cache: false,
+    success: function(result){
+      $.each(result, function() {
+        $('.explorer ul').append('<li><div class="audio">'+String(this)+'</div></li>');
+      });
+      initFiles();
+    }
+  });
+
+};
+
+var initFiles = function() {
+
+  $(".audio").each(function(){
+
+    var name = $(this).html();
+
+    $( this ).draggable({
+      revert: 'invalid',
+      helper: function(){
+        return '<div class="track">'+name+'</div>'
+      },
+      containment: 'document',
+      cursor: 'move'
+    });
+
+  });
+
+};
